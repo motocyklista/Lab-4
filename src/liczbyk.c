eaabab2ad689675b18dd0568c19a1c91296f35e0
Piotr <79946580+motocyklista@users.noreply.github.com>
2025-11-02 23:50:06 +0100

    Update liczbyk.c

--- src/liczbyk.c
+++ src/liczbyk.c
@@ -3,9 +3,13 @@
 int main(void) {
 
 int k;
+  
+// Wczytanie liczby k od użytkownika
 
 printf("Podaj liczbe k (wieksze od 0): ");
 scanf("%d", &k);
+
+// Sprawdzenie poprawności danych wejściowych
 
 while (k <= 0) {
     printf("Blad: liczba musi byc wieksza od 0!\n");
@@ -15,7 +19,12 @@ while (k <= 0) {
 
 printf("Wynik: \n");
 
+ // Sprawdź liczby od 50 do 100
+
 for (int i = 50; i <= 100; i++){
+
+    // Sprawdzenie, czy powyższe liczby są podzielne przez k
+
     if (i % k == 0)
     {
        printf("%d\n", i);
